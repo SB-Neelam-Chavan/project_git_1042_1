419f7c9e7931645d563d47315fd84ac108dd2b10
neelam.chavan@smartbear.com <neelam.chavan@smartbear.com>
2025-01-08 11:10:43 +0530

    Added 5 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-19805.stoplight-dev.com/api/v1/projects/cHJqOjgxMg/spectral.js?branch_slug=main&token=01e33561-8881-4096-a80f-0a5f002771c8";
+export default { extends: ruleset };
---
+++ docs/Article.md
@@ -0,0 +1,7 @@
+---
+stoplight-id: euef0g7n7onju
+---
+
+# Article
+
+The beginning of an awesome article...
